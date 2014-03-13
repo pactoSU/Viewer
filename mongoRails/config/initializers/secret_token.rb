842c3612a098ec5450141da7e37a3748e1479a5e
Anthony Marcheselli <anthony607@gmail.com>
2014-03-13 10:03:57 -0700

    SeaBear: presentation trimmings

---
+++ mongoRails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoRails::Application.config.secret_key_base = '93f2591d249c21cca561d4e5aed8db0edba9fcc4195f952df378cceee0e3b7584f53baeadf6e79f82a5d87a8f499777b7e0717cc1d35d379ab8e506e9eebdda1'
